50696494a6d8ef2d80c27011af3af2d8f4426a05
Md Irtiaz Kabir <codingislife.code@gmail.com>
2019-09-06 15:19:59 +0600

    fix

---
+++ navbar.js
@@ -0,0 +1,38 @@
+var navbar;
+var home,videos,aboutUs;
+function setup(){
+	noCanvas();
+	navbar = selectAll(".navbar");
+	for(let i = 0; i < navbar.length; i ++){
+		navbar[i].mouseOver(highlight);
+		navbar[i].mouseOut(unhighlight);
+	}
+	
+	home = navbar[0];
+	videos = navbar[1];
+	aboutUs = navbar[2];
+
+	home.mouseClicked(openHome);
+	videos.mouseClicked(openVideos);
+	aboutUs.mouseClicked(openAbout);
+}
+
+function highlight(){
+	this.style('background','yellow');
+}
+
+function unhighlight(){
+	this.style('background','');
+}
+
+function openHome(){
+	open('index.html','_self');
+}
+
+function openVideos(){
+	open('Videos.html','_self');
+}
+
+function openAbout(){
+	open('about.html','_self');
+}
